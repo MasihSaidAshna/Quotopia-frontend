58def68aba8e7715355b999d378ac027125e7965
Jothom2912 <johannes1504@gmail.com>
2023-12-05 15:22:17 +0100

    Update på adminpanel

--- src/Javascript/Test.js
+++ src/Javascript/Test.js
@@ -1,5 +1,6 @@
 window.onload = function() {
     const URLQuote = "http://localhost:8080/api/quote";
+    const URLGenre = "http://localhost:8080/api/genre";
 
     // Function to fetch quotes and populate the table
     function fetchAndPopulateQuotes() {
@@ -15,7 +16,25 @@ window.onload = function() {
             })
             .catch(error => {
                 // Handle errors
-                console.error('There was a problem with the fetch operation:', error);
+                console.error('There was a problem with fetching quotes:', error);
+            });
+    }
+
+    // Function to fetch genres and populate the table
+    function fetchAndPopulateGenres() {
+        fetch(URLGenre)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok.');
+                }
+                return response.json();
+            })
+            .then(data => {
+                makeGenreRows(data);
+            })
+            .catch(error => {
+                // Handle errors
+                console.error('There was a problem with fetching genres:', error);
             });
     }
 
@@ -26,19 +45,38 @@ window.onload = function() {
 
         const rows = quotes.map(quote => {
             return `
-                        <tr>
-                            <td>${quote.quoteID}</td>
-                            <td>${quote.quoteText}</td>
-                            <td>${quote.author ? quote.author.authorName : ''}</td> 
-                        </tr>
-                    `;
+                <tr>
+                    <td>${quote.quoteID}</td>
+                    <td>${quote.quoteText}</td>
+                    <td>${quote.author ? quote.author.authorName : ''}</td> 
+                </tr>
+            `;
         });
 
         tableBody.innerHTML = rows.join("");
     }
 
-    // Trigger the fetch and population of quotes when the page loads
+    // Function to create table rows for genres
+    function makeGenreRows(genres) {
+        const tableBody = document.getElementById("genre-table-body");
+        tableBody.innerHTML = ''; // Clear previous rows
+
+        const rows = genres.map(genre => {
+            return `
+                <tr>
+                    <td>${genre.genreID}</td>
+                    <td>${genre.genreName}</td>
+                </tr>
+            `;
+        });
+
+        tableBody.innerHTML = rows.join("");
+    }
+
+    // Trigger the fetch and population of both quotes and genres when the page loads
     fetchAndPopulateQuotes();
+    fetchAndPopulateGenres();
 };
+
 
 
