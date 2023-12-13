2d0668726f7c3f9187e3c0b6fbaca33c5b4c6d65
cihadozdemir <ciha0128@gmail.com>
2023-12-13 20:01:34 +0100

    login og singup lavet

--- src/js/login.js
+++ src/js/login.js
@@ -14,6 +14,13 @@ document.addEventListener("DOMContentLoaded", function () {
         const password = document.getElementById('signupPassword').value;
         signupUser(username, password);
     });
+
+    // Check if the user is already logged in
+    const authToken = localStorage.getItem('token');
+    if (authToken) {
+        // User is logged in, update the navigation bar
+        updateNavigationBar(true);
+    }
 });
 
 function loginUser(username, password) {
@@ -22,16 +29,19 @@ function loginUser(username, password) {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-
         },
-        body: JSON.stringify({username, password}),
+        body: JSON.stringify({ username, password }),
     })
         .then(response => response.json())
         .then(data => {
             // Handle the response, e.g., save the token to local storage
             console.log('Login successful:', data);
             localStorage.setItem('token', data.token);
-            window.location.href = '../html/newquotes.html';
+
+            $('#loginModal').modal('hide');
+
+            // Update the navigation bar after login
+            updateNavigationBar(true);
         })
         .catch(error => console.error('Login failed:', error));
 }
@@ -45,7 +55,7 @@ function signupUser(username, password) {
             'Content-Type': 'application/json',
             'Authorization': 'Bearer ' + authToken,
         },
-        body: JSON.stringify({ username, password } ),
+        body: JSON.stringify({ username, password }),
     })
         .then(response => {
             if (!response.ok) {
@@ -55,7 +65,47 @@ function signupUser(username, password) {
         })
         .then(data => {
             console.log('Signup successful:', data);
-            window.location.href = 'signup-success.html';
+            // Update the navigation bar after signup
+            updateNavigationBar(true);
         })
         .catch(error => console.error('Signup failed:', error.message));
+}
+
+function updateNavigationBar(isLoggedIn) {
+    // Update the navigation bar based on the user's logged-in state
+    const rightNavLinks = document.querySelectorAll('.navbar-nav.mr-auto li');
+    const authLinks = document.getElementById('authLinks');
+    const leftNavLinks = document.querySelectorAll('.navbar-nav.ml-auto li');
+    const newPageLink = document.createElement('li');
+    newPageLink.classList.add('nav-item');
+    newPageLink.innerHTML = '<a class="nav-link" href="../html/edit.html">Admin Panel</a>';
+
+    if (isLoggedIn) {
+        // User is logged in, update the navigation bar
+        rightNavLinks.forEach(link => link.style.display = 'none');
+        authLinks.innerHTML = '<li class="nav-item"><a class="nav-link" href="#" onclick="logoutUser()">Sign Out</a></li>';
+        leftNavLinks.forEach(link => link.style.display = 'inline-block');
+
+        // Add the new page link for logged-in users
+        document.querySelector('.navbar-nav').appendChild(newPageLink);
+    } else {
+        // User is not logged in, restore the original navigation bar
+        rightNavLinks.forEach(link => link.style.display = 'inline-block');
+        authLinks.innerHTML = '<li class="nav-item"><a class="nav-link mr-3" data-target="#loginModal" data-toggle="modal" href="#">Login</a></li>' +
+            '<li class="nav-item"><a class="nav-link" data-target="#signupModal" data-toggle="modal" href="#">Sign Up</a></li>'
+        leftNavLinks.forEach(link => link.style.display = 'none');
+
+        // Remove the new page link for non-logged-in users
+        if (newPageLink.parentNode) {
+            newPageLink.parentNode.removeChild(newPageLink);
+        }
+    }
+}
+
+function logoutUser() {
+    // Clear the token from local storage
+    localStorage.removeItem('token');
+
+    // Update the navigation bar after logout
+    updateNavigationBar(false);
 }
