3d2e7998cd660b27a93dd19a5525702a122727eb
Jothom2912 <johannes1504@gmail.com>
2023-12-14 19:03:46 +0100

    Searchbar update

--- src/js/searchbar.js
+++ src/js/searchbar.js
@@ -1,52 +1,20 @@
 document.addEventListener('DOMContentLoaded', async () => {
     try {
         const genres = await fetchAllGenres();
+        console.log('Fetched data:', genres);
         populateDropdownWithGenres(genres);
+
+        const dropdownItems = document.querySelectorAll('.dropdown-item');
+        dropdownItems.forEach(item => {
+            item.addEventListener('click', () => {
+                const selectedGenre = item.textContent.trim();
+                navigateToQuotesPage(selectedGenre);
+            });
+        });
     } catch (error) {
         console.error('There was a problem with fetching genres:', error);
     }
-
-    const searchButton = document.getElementById('searchButton');
-    searchButton.addEventListener('click', async () => {
-        const selectedGenre = document.getElementById('searchInput').value;
-        try {
-            const filteredQuotes = await filterQuotesByGenre(selectedGenre);
-            console.log(filteredQuotes);
-            // Display or manipulate the filtered quotes as needed
-        } catch (error) {
-            console.error('There was a problem with fetching quotes by genre:', error);
-        }
-    });
-
-    // Event listener for dropdown genre selection
-    const genreDropdown = document.getElementById('genreDropdown');
-    if (genreDropdown) {
-        genreDropdown.addEventListener('click', event => {
-            const selectedGenre = event.target.textContent;
-            document.getElementById('searchInput').value = selectedGenre;
-        });
-    } else {
-        console.error('Dropdown element not found');
-    }
 });
-
-function populateDropdownWithGenres(genres) {
-    const genreDropdown = document.getElementById('genreDropdown');
-    if (!genreDropdown) {
-        console.error('Dropdown element not found');
-        return;
-    }
-
-    genreDropdown.innerHTML = '';
-    genres.forEach(genre => {
-        const genreItem = document.createElement('a');
-        genreItem.classList.add('dropdown-item');
-        genreItem.href = '#';
-        genreItem.textContent = genre;
-        genreDropdown.appendChild(genreItem);
-    });
-}
-
 
 async function fetchAllGenres() {
     const URLGenres = "http://localhost:8080/api/genre";
@@ -57,26 +25,40 @@ async function fetchAllGenres() {
             throw new Error('Network response was not ok. Status: ' + response.status);
         }
         const data = await response.json();
-
-        return data.genre; // Assuming the API returns an array of genres
+        const genres = data || [];
+        return genres;
     } catch (error) {
         console.error('There was a problem with fetching genres:', error);
         return [];
     }
 }
 
-async function filterQuotesByGenre(genre) {
-    const URLQuoteByGenre = `http://localhost:8080/api/quote?genre=${encodeURIComponent(genre)}`;
+function populateDropdownWithGenres(genres) {
+    const genreDropdown = document.getElementById('genreDropdown');
 
-    try {
-        const response = await fetch(URLQuoteByGenre);
-        if (!response.ok) {
-            throw new Error('Network response was not ok. Status: ' + response.status);
-        }
-        const data = await response.json();
-        return data.quotes; // Assuming the API returns an array of quotes for the given genre
-    } catch (error) {
-        console.error('There was a problem with fetching quotes by genre:', error);
-        return [];
+    if (!genreDropdown || !Array.isArray(genres) || genres.length === 0) {
+        console.error('Dropdown element not found or genres empty');
+        return;
     }
+
+    function handleGenreItemClick(genre) {
+        return function () {
+            navigateToQuotesPage(genre.genreName);
+        };
+    }
+
+    genres.forEach(genre => {
+        const genreItem = document.createElement('div');
+        genreItem.classList.add('dropdown-item');
+        genreItem.textContent = genre.genreName;
+
+        genreItem.addEventListener('click', handleGenreItemClick(genre));
+
+        genreDropdown.appendChild(genreItem);
+    });
 }
+
+function navigateToQuotesPage(genre) {
+    window.location.href = `Category.html?genre=${encodeURIComponent(genre)}`;
+}
+
